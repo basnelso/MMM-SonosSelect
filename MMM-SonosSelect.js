0e53aeb174ca443f8c8a2cd5f954f5421450a591
Brady Snelson <bsnelson@umd.edu>
2021-03-04 16:12:05 -0500

    small fix

---
+++ MMM-SonosSelect.js
@@ -0,0 +1,146 @@
+/* global Module */
+
+/* Magic Mirror 2
+ * Module: MMM-Modulebar
+ *
+ * By Erik Pettersson
+ * Based on the TouchNavigation Module by Brian Janssen
+ *
+ * MIT Licensed.
+ */
+
+const request = require('request');
+
+Module.register("MMM-SonosSelect",{
+	
+	requiresVersion: "2.1.0",
+	
+    defaults: {
+        // Allow the module to force modules to be shown (if hidden and locked by another module ex. profile-switcher).
+        allowForce: false,
+        // Determines if the border around the buttons should be shown.
+        showBorder: true,
+        // The minimum width for all the buttons.
+        minWidth: "0px",
+        // The minimum height for all the buttons.
+        minHeight: "0px",
+        // The location of the symbol relative to the text. Options: left, right, top or bottom
+        picturePlacement: "left",
+        // The direction of the bar. Options: row, column, row-reverse or column-reverse
+        direction: "row",
+		// The speed of the hide and show animation.
+		animationSpeed: 1000,
+		// IP address of server hosting sonos api
+		server_ip: "http://localhost:5005",
+        // The default button 1. Add your buttons in the config.
+		buttons: {
+            "1": {
+				room: "Living Room",
+                symbol: "couch"
+            },
+			"2": {
+				room: "Kitchen",
+                symbol: "utensils"
+            },
+			"3": {
+				room: "Bathroom",
+                symbol: "toilet"
+            },
+		}
+    },
+
+    // Define required styles.
+	getStyles: function(){
+		return ["font-awesome.css", "MMM-Modulebar.css"];
+	},
+
+    // Override dom generator.
+    getDom: function() {
+        var menu = document.createElement("span");
+        menu.className = "modulebar-menu";
+        menu.id = this.identifier + "_menu";
+        menu.style.flexDirection = this.config.direction;
+		// Sends each button to the "createButton" function be created.
+		for (var num in this.config.buttons) {
+			menu.appendChild(this.createButton(this, num, this.config.buttons[num], this.config.picturePlacement));
+        }
+
+        return menu;
+    },
+
+	// Creates the buttons.
+    createButton: function (self, num, data, placement) {
+		// Creates the span element to contain all the buttons.
+		var item = document.createElement("span");
+        // Builds a unique identity / button.
+		item.id = self.identifier + "_button_" + num;
+        // Sets a class to all buttons.
+		item.className = "modulebar-button";
+        // Makes sure the width and height is at least the defined minimum.
+		item.style.minWidth = self.config.minWidth;
+        item.style.minHeight = self.config.minHeight;
+		// When a button is clicked, the room either gets grouped/ungrouped depending on its status.
+		item.addEventListener("click", function () {
+			// Request the current state of the sonos system.
+			request.get(this.config.server_ip + this.config.buttons[num].room + '/playpause');
+		});
+		// Fixes the aligning.
+        item.style.flexDirection = {
+            "right"  : "row-reverse",
+            "left"   : "row",
+            "top"    : "column",
+            "bottom" : "column-reverse"
+        }[placement];
+		// Sets the border around the symbol/picture/text to black.
+        if (!self.config.showBorder) {
+            item.style.borderColor = "black";
+        }
+		// Adds the Font-Awesome symbol if specified.
+        if (data.symbol) {
+            var symbol = document.createElement("span");
+            symbol.className = "modulebar-picture fa fa-" + data.symbol;
+			// Sets the size on the symbol if specified.
+            if (data.size) {
+                symbol.className += " fa-" + data.size;
+                symbol.className += data.size == 1 ? "g" : "x";
+            }
+			// Align the symbol with a margin.
+            if (data.text && placement === "left") {
+                symbol.style.marginRight = "4px";
+            }
+			// Adds the symbol to the item.
+            item.appendChild(symbol);
+
+		// Adds a picture if specified.
+		} else if (data.img) {
+            var image = document.createElement("img");
+            image.className = "modulebar-picture";
+            image.src = data.img;
+			// Sets the size of the picture if specified.
+            if (data.width)  image.width  = data.width;
+            if (data.height) image.height = data.height;
+			// Align the picture with a margin.
+            if (data.text && placement === "left") {
+                image.style.marginRight = "4px";
+            }
+			// Adds the picture to the item.
+            item.appendChild(image);
+        }
+		// Adds the text if specified.
+        if (data.text) {
+            var text = document.createElement("span");
+            text.className = "modulebar-text";
+            text.innerHTML = data.text;
+			// Align the text with a margin.
+            if ((data.symbol || data.img) && placement === "right") {
+                text.style.marginRight = "4px";
+            }
+			// Adds the text to the item.
+            item.appendChild(text);
+        }
+		// All done. :)
+        return item;
+    }
+});	
+
+
